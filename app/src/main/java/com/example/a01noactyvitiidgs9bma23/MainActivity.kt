a751489464f90d46dfc0961a7131df250fe82501
josLuna <jos.luna_santillan@hotmail.com>
2023-07-04 14:25:01 -0600

    04/07/23 DLSJ

--- app/src/main/java/com/example/a01noactyvitiidgs9bma23/MainActivity.kt
+++ app/src/main/java/com/example/a01noactyvitiidgs9bma23/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.a01noactyvitiidgs9bma23
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
-import androidx.lifecycle.MutableLiveData
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.a01noactyvitiidgs9bma23.databinding.ActivityMainBinding
 
@@ -16,8 +16,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var model: MainActivityViewModel
     private var numero:Int=0
+    private var botonVisible: Boolean = false
+
+    private var visible:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         model= ViewModelProvider(this).get(MainActivityViewModel::class.java)
         binding= ActivityMainBinding.inflate(layoutInflater)
         var view = binding.root
@@ -48,6 +52,13 @@ class MainActivity : AppCompatActivity() {
                 else -> R.drawable.dice_6
             })
         })
+        model.numero.observe(this,{
+            numero=it.toInt()
+        })
+        model.diceNombre.observe(this,{
+            binding.edtUserName.text = it.toString()
+        })
+
         binding.btnUserName?.setOnClickListener{
             addUserName(it)
         }
@@ -55,11 +66,45 @@ class MainActivity : AppCompatActivity() {
             updateUsername(it)
         }
         binding.reset.setOnClickListener { updateUsername(it) }
+       model.visible1.observe(this,{
+           visible  = it.toInt()
+        })
+
+
+
+        if(visible == 0){
+            if (!botonVisible) {
+                binding.btnUserName.visibility = View.VISIBLE
+                binding.tvUserName?.visibility=View.VISIBLE
+            }
+        }
 
     }
     /**
      * para ver todos los metodos de vida solo es precionar ctrl + o
      * */
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("botonVisible", botonVisible)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        botonVisible = savedInstanceState.getBoolean("botonVisible")
+        if(visible == 0){
+            if (!botonVisible) {
+                binding.btnUserName.visibility = View.VISIBLE
+                binding.tvUserName?.visibility=View.VISIBLE
+                binding.edtUserName?.visibility= View.GONE
+            }
+        }else{
+            if (!botonVisible) {
+                binding.btnUserName.visibility = View.GONE
+                binding.tvUserName?.visibility=View.GONE
+                binding.edtUserName?.visibility= View.VISIBLE
+            }
+        }
+    }
     private fun rollDice(){
         binding.imageView.setVisibility(View.VISIBLE)
         binding.imageView3.setVisibility(View.VISIBLE)
@@ -68,21 +113,25 @@ class MainActivity : AppCompatActivity() {
         model.setDice1Number(randomInt)
         model.setDice2Number(randomInt2)
         numero++
+        model.setNumero(numero)
 
     }
     private fun time(){
-       Toast.makeText(this,"Has tirado ${numero} veces los dados",Toast.LENGTH_SHORT).show()
+       Toast.makeText(this,"${binding.edtUserName.text} has tirado ${numero} veces los dados",Toast.LENGTH_SHORT).show()
     }
 
     private fun updateUsername(view:View?){
         numero = 0
         binding.tvMainAct.text = "0"
         binding.textView.text = "0"
+        var visible2 = 0
+        model.setVisible(visible2)
         binding.imageView.setVisibility(View.INVISIBLE)
         binding.imageView3.setVisibility(View.INVISIBLE)
         binding.tvUserName?.visibility=View.VISIBLE
         binding.btnUserName?.visibility=View.VISIBLE
         binding.edtUserName?.visibility=View.GONE
+
         //view?.visibility=View.GONE
        binding.tvUserName?.requestFocus()
 
@@ -90,7 +139,9 @@ class MainActivity : AppCompatActivity() {
         showKbd.showSoftInput(binding.edtUserName,1)
     }
     private fun addUserName(view:View?) {
-        model.userName(binding.tvUserName?.text)
+        model.userName(binding.tvUserName.text.toString())
+        var visible2 = 2
+        model.setVisible(visible2)
         binding.btnUserName?.visibility=View.GONE
         binding.tvUserName?.visibility=View.GONE
         //view?.visibility=View.GONE
@@ -100,4 +151,5 @@ class MainActivity : AppCompatActivity() {
         val hideKbd=getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         hideKbd.hideSoftInputFromWindow(view?.windowToken,0)
     }
+
 }
--- app/src/main/java/com/example/a01noactyvitiidgs9bma23/MainActivityViewModel.kt
+++ app/src/main/java/com/example/a01noactyvitiidgs9bma23/MainActivityViewModel.kt
@@ -1,15 +1,15 @@
 package com.example.a01noactyvitiidgs9bma23
 
-import android.text.Editable
-import androidx.lifecycle.LiveData
+import android.widget.Button
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel:ViewModel() {
     var dice1Number: MutableLiveData<Int> = MutableLiveData<Int>().apply { value=0 }
     var dice2Number: MutableLiveData<Int> = MutableLiveData<Int>().apply { value=0 }
-    var diceNomre: MutableLiveData<String> = MutableLiveData<String>().apply { "Nombre Usuario" }
     var numero:MutableLiveData<Int> = MutableLiveData<Int>().apply { value=0 }
+    var diceNombre: MutableLiveData<String> = MutableLiveData<String>().apply { value="Usuario" }
+    var visible1: MutableLiveData<Int> = MutableLiveData<Int>().apply { value=0 }
 
     fun setDice1Number(value:Int?){
         this.dice1Number.value  =   value
@@ -18,9 +18,17 @@ class MainActivityViewModel:ViewModel() {
     fun setDice2Number(value:Int?){
         this.dice2Number.value  =   value
     }
-
-    fun userName(value: Editable?){
-        this.diceNomre.value = value.toString()
+    fun userName(value: String){
+        this.diceNombre.value = value
+    }
+    fun setNumero(value: Int?){
+        this.numero.value = value
+    }
+    fun setVisible(value: Int?){
+        this.visible1.value = value
+    }
+    fun getDice1Number (): Int?{
+        return dice1Number.value
     }
 
 
